0849b15308619ef0010526aeae305953b09569c3
ony <onydarrell@gmail.com>
2025-02-17 23:48:24 +0700

    Update db_music.sql

--- db/db_music.sql
+++ db/db_music.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 17 Feb 2025 pada 09.30
+-- Waktu pembuatan: 17 Feb 2025 pada 17.48
 -- Versi server: 10.4.32-MariaDB
 -- Versi PHP: 8.2.12
 
@@ -30,16 +30,20 @@ SET time_zone = "+00:00";
 CREATE TABLE `songs` (
   `id` int(11) NOT NULL,
   `title` varchar(255) NOT NULL,
-  `artist` varchar(255) NOT NULL,
-  `file_path` varchar(255) NOT NULL
+  `artist` varchar(255) DEFAULT NULL,
+  `file_path` text NOT NULL,
+  `duration` varchar(10) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data untuk tabel `songs`
 --
 
-INSERT INTO `songs` (`id`, `title`, `artist`, `file_path`) VALUES
-(1, 'Zona Nyaman', 'Fourtwnty', 'uploads/1739781004_Fourtwnty-ZonaNyaman.mp3');
+INSERT INTO `songs` (`id`, `title`, `artist`, `file_path`, `duration`) VALUES
+(1, 'Zona Nyaman', 'Fourtwnty', 'uploads/1739781004_Fourtwnty-ZonaNyaman.mp3', NULL),
+(3, 'Midnights', 'Taylor Swift', 'uploads/1739810442_Taylor Swift - Midnights.mp3', NULL),
+(4, 'Shallow', 'Lady Gaga ft Bradley Cooper', 'uploads/1739810537_Lady Gaga Ft Bradley Cooper - Shallow.mp3', NULL),
+(5, 'The Only Exception', 'Paramore', 'uploads/1739810602_Paramore - The Only Exception.mp3', NULL);
 
 --
 -- Indexes for dumped tables
@@ -49,7 +53,9 @@ INSERT INTO `songs` (`id`, `title`, `artist`, `file_path`) VALUES
 -- Indeks untuk tabel `songs`
 --
 ALTER TABLE `songs`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`id`),
+  ADD KEY `idx_title` (`title`),
+  ADD KEY `idx_artist` (`artist`);
 
 --
 -- AUTO_INCREMENT untuk tabel yang dibuang
@@ -59,7 +65,7 @@ ALTER TABLE `songs`
 -- AUTO_INCREMENT untuk tabel `songs`
 --
 ALTER TABLE `songs`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
